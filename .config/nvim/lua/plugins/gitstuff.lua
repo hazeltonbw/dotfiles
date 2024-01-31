da20f5a38ff9bd2166c49d31d9cc68235cc5b8f6
Brandon Hazelton <hazeltonbw@gmail.com>
2024-01-31 03:53:02 -0800

    Changed nVim config for more Git goodies

--- .config/nvim/lua/plugins/gitstuff.lua
+++ .config/nvim/lua/plugins/gitstuff.lua
@@ -7,8 +7,15 @@ return {
   config = function()
     require("gitsigns").setup()
 
-    vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+    vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", {})
     vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+    vim.keymap.set("n", "<leader>gb", ":Git blame<CR>", {})
+    vim.keymap.set("n", "<leader>ga", ":Git add .<CR>", {})
+    vim.keymap.set("n", "<leader>gc", ":Git commit<CR>", {})
+    vim.keymap.set("n", "<leader>gp", ":Git push<CR>", {})
+    vim.keymap.set("n", "<leader>gP", ":Git pull<CR>", {})
+    vim.keymap.set("n", "<leader>gl", ":Git log<CR>", {})
+    vim.keymap.set("n", "<leader>gs", ":Git status<CR>", {})
   end
   }
 }
